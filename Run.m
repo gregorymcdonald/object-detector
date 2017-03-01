7f60593ffedeadcff1cc8ec6af3b547bdaa03733
EpsilonF <greg.mcd.social@gmail.com>
2017-02-28 20:42:08 -0600

    Added simple run code.

--- Run.m
+++ Run.m
@@ -1,2 +1,10 @@
-% Run project
-disp('Nothing to do.');
+% Load test image
+inputImage = imread('resources/testCard.jpg');
+
+% Plot results
+numPlotRows = 1;
+numPlotColumns = 2;
+
+subplot(numPlotRows, numPlotColumns, 1);
+imshow(inputImage);
+title('Input Image');
